0526f9d0f4e194c5297a40e1c9d920160a069cda
florianldt <florian.ludot@gmail.com>
2021-06-20 22:09:56 +0200

    Create connect.js

---
+++ www/pages/connect.js
@@ -0,0 +1,31 @@
+import { generateOauthState } from '../config';
+
+function Connect() {
+    const oauthState = generateOauthState();
+    function onLineClick() {
+        window.location.assign(
+            `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_OAUTH_LINE_CHANNEL_ID}&redirect_uri=${process.env.NEXT_PUBLIC_OAUTH_LINE_REDIRECT_URI}&state=${oauthState}&scope=profile%20openid`,
+        );
+    }
+
+    return (
+        <div className="container">
+            <button className="line-button" onClick={onLineClick}>
+                Login with Line
+            </button>
+            <style jsx>{`
+                .container {
+                    padding: 80px;
+                }
+                .line-button {
+                    padding: 10px 20px;
+                    background-color: #33ac24;
+                    color: #fff;
+                    cursor: pointer;
+                }
+            `}</style>
+        </div>
+    );
+}
+
+export default Connect;
