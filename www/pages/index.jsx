68c2ab6186e020d3374bbb4905b8980182441e28
florianldt <florian.ludot@gmail.com>
2021-06-21 00:38:45 +0200

    add link to /connect on index

--- www/pages/index.jsx
+++ www/pages/index.jsx
@@ -1,7 +1,14 @@
+// #region Global Imports
+import Link from 'next/link';
+// #endregion Global Imports
+
 function Home() {
     return (
         <div>
             <h1>www</h1>
+            <Link href="/connect">
+                <a>/connect</a>
+            </Link>
         </div>
     );
 }
